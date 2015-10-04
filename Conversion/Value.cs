fe6b301773f2b6ebb72be5bff5aa0da6a900d18d
Namankevi4 <namankevi4@gmail.com>
2015-10-05 02:13:14 +0300

    create some values of SI

---
+++ Conversion/Value.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conversion
+{
+    public class Value
+    {
+        public int val;
+        public string name;
+
+
+        public static Value operator *(Value t, Value s)
+        {
+            var m = new Value();
+            if (t.GetType() == s.GetType())
+            {
+                Type ty = t.GetType();
+                
+                m.val = t.val * s.val;
+                return (Value)m;
+            }
+           
+            
+            m.name = "m";
+            
+            return m;
+        }
+    }
+}
